355ef9ba2b202cd8a9e0be4ab9f1e3011d8bcb02
saeedjhn <saeedjahanakbari@gmail.com>
2024-05-16 10:46:09 +0330

    chore: Implement postgresql connection

---
+++ internal/infrastructure/db/postgresql/conn.go
@@ -0,0 +1,40 @@
+package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"time"
+)
+
+type DB interface {
+	Conn() *sql.DB
+}
+
+type PostgresqlDB struct {
+	cfg Config
+	db  *sql.DB
+}
+
+func New(config Config) *PostgresqlDB {
+	cnn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Tehran",
+		config.Host, config.Port, config.Username, config.Password,
+		config.Database, config.SSLMode)
+
+	db, err := sql.Open("postgres", cnn)
+	if err != nil {
+		log.Fatalf("can`t open postgres connection: %v", err)
+	}
+
+	// See "Important config" section
+	db.SetMaxIdleConns(config.MaxIdleConns)
+	db.SetMaxOpenConns(config.MaxOpenConns)
+	db.SetConnMaxLifetime(config.ConnMaxLiftTime * time.Second)
+
+	return &PostgresqlDB{cfg: config, db: db}
+}
+
+func (m *PostgresqlDB) Conn() *sql.DB {
+	return m.db
+}
