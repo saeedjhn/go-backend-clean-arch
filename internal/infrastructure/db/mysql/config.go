9a1ffa725d11a192a719e90af49e36ef5d0f66ed
saeedjhn <saeedjahanakbari@gmail.com>
2024-05-16 10:46:26 +0330

    chore: Implement mysql connection

---
+++ internal/infrastructure/db/mysql/config.go
@@ -0,0 +1,15 @@
+package mysql
+
+import "time"
+
+type Config struct {
+	Host            string        `mapstructure:"host"`
+	Port            string        `mapstructure:"port"`
+	Username        string        `mapstructure:"username"`
+	Password        string        `mapstructure:"password"`
+	Database        string        `mapstructure:"database"`
+	SSLMode         string        `mapstructure:"ssl_mode"`
+	MaxIdleConns    int           `mapstructure:"max_idle_conns"`
+	MaxOpenConns    int           `mapstructure:"max_open_conns"`
+	ConnMaxLiftTime time.Duration `mapstructure:"conn_max_life_time"`
+}
---
+++ internal/infrastructure/db/mysql/conn.go
@@ -0,0 +1,38 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+)
+
+type DB interface {
+	Conn() *sql.DB
+}
+
+type MySqlDB struct {
+	cfg Config
+	db  *sql.DB
+}
+
+func New(config Config) *MySqlDB {
+	conn := fmt.Sprintf("%s:%s@(%s:%s)/%s?parseTime=true",
+		config.Username, config.Password, config.Host, config.Port, config.Database)
+
+	db, err := sql.Open("mysql", conn)
+	if err != nil {
+		log.Fatalf("can't open mysql db: %v", err)
+	}
+
+	// See "Important settings" section.
+	db.SetMaxIdleConns(config.MaxIdleConns)
+	db.SetMaxOpenConns(config.MaxOpenConns)
+	db.SetConnMaxLifetime(config.ConnMaxLiftTime * time.Second)
+
+	return &MySqlDB{cfg: config, db: db}
+}
+
+func (m *MySqlDB) Conn() *sql.DB {
+	return m.db
+}
