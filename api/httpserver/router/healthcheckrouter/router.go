01f22485467d130db25848233e1e144ab153fbff
saeedjhn <saeedjahanakbari@gmail.com>
2024-05-16 13:53:31 +0330

    feat: Added health check router in app

---
+++ api/httpserver/router/healthcheckrouter/router.go
@@ -0,0 +1,18 @@
+package healthcheckrouter
+
+import (
+	"github.com/labstack/echo/v4"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/bootstrap"
+	"net/http"
+)
+
+func New(app *bootstrap.Application, e *echo.Echo) {
+	g := e.Group("health-check")
+	{
+		g.GET("/", func(c echo.Context) error {
+			return c.JSON(http.StatusOK, echo.Map{
+				"message": "everything is good!",
+			})
+		})
+	}
+}
--- api/httpserver/router/router.go
+++ api/httpserver/router/router.go
@@ -2,24 +2,17 @@ package router
 
 import (
 	"github.com/labstack/echo/v4"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/api/httpserver/router/healthcheckrouter"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/api/httpserver/router/taskrouter"
 	"go-backend-clean-arch-according-to-go-standards-project-layout/api/httpserver/router/userrouter"
 	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/bootstrap"
 )
-
-//func Setup(
-//	cfg *configs.Config,
-//	mysqlDB *mysql.MySqlDB,
-//	postgresqlDB *postgresql.PostgresqlDB,
-//	e *echo.Echo,
-//) {
-//	userrouter.New(cfg, postgresqlDB, e)
-//	//taskrouter.New(cfg, mysqlDB, e)
-//}
 
 func Setup(
 	app *bootstrap.Application,
 	e *echo.Echo,
 ) {
 	userrouter.New(app, e)
-	//taskrouter.New(cfg, mysqlDB, e)
+	taskrouter.New(app, e)
+	healthcheckrouter.New(app, e)
 }
