315acde85a734d650d7574c957645e78a2cc76b0
saeedjhn <saeedjahanakbari@gmail.com>
2024-05-16 10:43:23 +0330

    chore: Implement task router

--- api/httpserver/router/taskrouter/router.go
+++ api/httpserver/router/taskrouter/router.go
@@ -1,1 +1,17 @@
 package taskrouter
+
+import (
+	"github.com/labstack/echo/v4"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/configs"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/infrastructure/db/mysql"
+	"net/http"
+)
+
+func New(cfg *configs.Config, mysqlDB *mysql.MySqlDB, e *echo.Echo) {
+	g := e.Group("tasks")
+	{
+		g.GET("/", func(c echo.Context) error {
+			return c.JSON(http.StatusOK, "-- tasks --")
+		})
+	}
+}
