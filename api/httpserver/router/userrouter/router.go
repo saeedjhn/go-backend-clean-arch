20c8cdf71f9740f41095a218a715f4724c87d149
saeedjhn <saeedjahanakbari@gmail.com>
2024-05-16 10:43:09 +0330

    chore: Implement user router

--- api/httpserver/router/userrouter/router.go
+++ api/httpserver/router/userrouter/router.go
@@ -1,1 +1,39 @@
 package userrouter
+
+import (
+	"github.com/labstack/echo/v4"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/api/httpserver/handler/userhandler"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/bootstrap"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/gateway/taskgateway"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/gateway/usergateway"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/repository/taskrepository/postgresqltask"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/repository/userrespository/postgresqluser"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/usecase/taskusecase"
+	"go-backend-clean-arch-according-to-go-standards-project-layout/internal/usecase/userusecase"
+)
+
+func New(
+	app *bootstrap.Application,
+	e *echo.Echo,
+) {
+	// Repository
+	taskPostgresql := postgresqltask.New(app.PostgresqlDB)
+	userPostgresql := postgresqluser.New(app.PostgresqlDB)
+
+	// Gateway & Usecase
+	taskGateway := taskgateway.New(taskPostgresql)
+	taskUsecase := taskusecase.New(taskGateway)
+
+	// Gateway & Usecase
+	userGateway := usergateway.New(userPostgresql, taskUsecase)
+	userUsecase := userusecase.New(userGateway)
+
+	// Handler
+	userHandler := userhandler.New(app, userUsecase)
+
+	g := e.Group("users")
+	{
+		g.POST("/", userHandler.Register)
+		g.GET("/task-list", userHandler.TaskList)
+	}
+}
