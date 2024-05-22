10e30796a840eaef7bd68b031baff7362e434114
saeedjhn <saeedjahanakbari@gmail.com>
2024-05-22 13:49:58 +0330

    fix: Fixed redis config

--- internal/infrastructure/persistance/cache/redis/config.go
+++ internal/infrastructure/persistance/cache/redis/config.go
@@ -3,14 +3,14 @@ package redis
 import "time"
 
 type Config struct {
-	Host               string        `env:"host"`
-	Port               string        `env:"port"`
-	Password           string        `env:"password"`
-	DB                 int           `env:"db"`
-	PoolSize           int           `env:"pool_size"`
-	DialTimeout        time.Duration `env:"dial_timeout"`
-	ReadTimeout        time.Duration `env:"read_timeout"`
-	WriteTimeout       time.Duration `env:"write_timeout"`
-	PoolTimeout        time.Duration `env:"pool_timeout"`
-	IdleCheckFrequency time.Duration `env:"idle_check_frequency"`
+	Host               string        `mapstructure:"host"`
+	Port               string        `mapstructure:"port"`
+	Password           string        `mapstructure:"password"`
+	DB                 int           `mapstructure:"db"`
+	PoolSize           int           `mapstructure:"pool_size"`
+	DialTimeout        time.Duration `mapstructure:"dial_timeout"`
+	ReadTimeout        time.Duration `mapstructure:"read_timeout"`
+	WriteTimeout       time.Duration `mapstructure:"write_timeout"`
+	PoolTimeout        time.Duration `mapstructure:"pool_timeout"`
+	IdleCheckFrequency time.Duration `mapstructure:"idle_check_frequency"`
 }
