ffa4e6f063aa1ff089de6fc2b5e8dcc2493cf6bf
saeedjhn <saeedjahanakbari@gmail.com>
2024-06-08 17:00:35 +0330

    chore: Refactor user gateway

--- internal/gateway/usergateway/user.go
+++ internal/gateway/usergateway/user.go
@@ -4,32 +4,32 @@ import (
 	"go-backend-clean-arch/internal/domain"
 )
 
-type AuthInteractor interface {
+type AuthContract interface {
 	CreateAccessToken(user domain.User) (string, error)
 	CreateRefreshToken(user domain.User) (string, error)
 }
 
-type TaskInteractor interface {
-	TasksForUser(userId uint) ([]domain.Task, error)
+type TaskContract interface {
+	TasksForUser(userID uint) ([]domain.Task, error)
 }
 
 type UserGateway struct {
-	authInteractor AuthInteractor
-	taskInteractor TaskInteractor
+	auth AuthContract
+	task TaskContract
 }
 
-func New(authInteractor AuthInteractor, taskInteractor TaskInteractor) *UserGateway {
-	return &UserGateway{authInteractor: authInteractor, taskInteractor: taskInteractor}
+func New(authInteractor AuthContract, taskInteractor TaskContract) *UserGateway {
+	return &UserGateway{auth: authInteractor, task: taskInteractor}
 }
 
 func (u *UserGateway) Tasks(userID uint) ([]domain.Task, error) {
-	return u.taskInteractor.TasksForUser(userID)
+	return u.task.TasksForUser(userID)
 }
 
 func (u *UserGateway) CreateAccessToken(user domain.User) (string, error) {
-	return u.authInteractor.CreateAccessToken(user)
+	return u.auth.CreateAccessToken(user)
 }
 
 func (u *UserGateway) CreateRefreshToken(user domain.User) (string, error) {
-	return u.authInteractor.CreateRefreshToken(user)
+	return u.auth.CreateRefreshToken(user)
 }
