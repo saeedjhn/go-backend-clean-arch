2b411478ba6f0a85cd9ae2043c879216046568f5
saeedjhn <saeedjahanakbari@gmail.com>
2024-09-12 09:01:18 +0330

    refactor: Refactor 'Pointers to Interfaces' for 'presentation layer application based on 'Uber go style guide'

--- internal/validator/taskvalidator/validator.go
+++ internal/validator/taskvalidator/validator.go
@@ -1,7 +1,11 @@
 package taskvalidator
 
+import "github.com/saeedjhn/go-backend-clean-arch/api/httpserver/handler/taskhandler"
+
 type Validator struct {
 }
+
+var _ taskhandler.Validator = (*Validator)(nil)
 
 func New() Validator {
 	return Validator{}
--- internal/validator/uservalidator/validator.go
+++ internal/validator/uservalidator/validator.go
@@ -1,7 +1,11 @@
 package uservalidator
 
+import "github.com/saeedjhn/go-backend-clean-arch/api/httpserver/handler/userhandler"
+
 type Validator struct {
 }
+
+var _ userhandler.Validator = (*Validator)(nil)
 
 func New() Validator {
 	return Validator{}
