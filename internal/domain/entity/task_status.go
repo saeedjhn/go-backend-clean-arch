a37b5480748fa7f8642e96c502d9add21a18d8ce
saeedjhn <saeedjahanakbari@gmail.com>
2024-09-12 15:53:12 +0330

    chore: Move 'TaskStatus' from 'task.go' to 'task_status.go' in domain layer application

---
+++ internal/domain/entity/task_status.go
@@ -0,0 +1,47 @@
+package entity
+
+/*
+Since variables have a 0 default value, you should usually start your enums on a non-zero value.
+There are cases where using the zero value makes sense, for example when the zero value case is the
+desirable default behavior.
+*/
+
+type TaskStatus uint8
+
+const (
+	Pending TaskStatus = iota + 1
+	InProgress
+	Completed
+)
+
+const (
+	PendingStr    = "pending"
+	InProgressStr = "in_progress"
+	CompletedStr  = "completed"
+)
+
+func (s TaskStatus) String() string {
+	switch s {
+	case Pending:
+		return PendingStr
+	case InProgress:
+		return InProgressStr
+	case Completed:
+		return CompletedStr
+	}
+
+	return ""
+}
+
+func MapToTaskStatus(status string) TaskStatus {
+	switch status {
+	case PendingStr:
+		return Pending
+	case InProgressStr:
+		return InProgress
+	case CompletedStr:
+		return Completed
+	}
+
+	return TaskStatus(0)
+}
