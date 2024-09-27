1f5462cb74b8928733461716d3e8f8da15516c9f
saeedjhn <saeedjahanakbari@gmail.com>
2024-09-27 08:37:25 +0330

    chore: Change 'mocks' to 'mocks_test' in 'taskservice' service layer

--- internal/service/taskservice/mocks_test/service_mock_test.go
+++ internal/service/taskservice/mocks_test/service_mock_test.go
@@ -1,21 +1,23 @@
-package mocks
+package mockstest_test
 
 import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/golang/mock/gomock"
 	"github.com/saeedjhn/go-backend-clean-arch/internal/domain/dto/servicedto/usertaskservicedto"
 	"github.com/saeedjhn/go-backend-clean-arch/internal/domain/entity"
 	"github.com/saeedjhn/go-backend-clean-arch/internal/service/taskservice"
-	"github.com/stretchr/testify/assert"
+	mockstest "github.com/saeedjhn/go-backend-clean-arch/internal/service/taskservice/mocks_test"
 )
 
 func TestCreate(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	mockRepository := NewMockRepository(ctrl)
+	mockRepository := mockstest.NewMockRepository(ctrl)
 
 	service := taskservice.New(mockRepository)
 
@@ -46,10 +48,23 @@ func TestCreate(t *testing.T) {
 		Status:      createTask.Status,
 	})
 
-	assert.NoError(t, err)
-	assert.NotNil(t, task.Task)
-	assert.Equal(t, uint(1), task.Task.ID)
-	assert.Equal(t, "My title", createdTask.Title)
+	// The difference between assert and require in Testify
+
+	// assert: Checks the condition, but if it fails, test execution continues.
+	// That is, if the condition is not checked, the test will continue.
+	// require: Checks the condition and stops the test immediately if it fails.
+
+	// Why is it recommended to use require for errors?
+	// In many cases, when an error occurs, the point of continuing to run the test is lost
+	// because the main condition of the test is not properly established.
+	// Using require in these cases will stop the test in the event of an error and
+	// prevent the execution of the rest of the test codes.
+	// This will help you avoid getting redundant and confusing error messages.۰
+
+	require.NoError(t, err)
+	require.NotNil(t, task.Task)
+	require.Equal(t, uint(1), task.Task.ID)
+	require.Equal(t, "My title", createdTask.Title)
 
 	// More
 }
