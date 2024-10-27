05ce867bb74612f0cdc61b9ff3984144c1acd93d
saeedjhn <saeedjahanakbari@gmail.com>
2024-10-27 21:18:09 +0330

    chore: Imple 'gRPC' server

--- api/v1/delivery/grpc/server.go
+++ api/v1/delivery/grpc/server.go
@@ -4,43 +4,48 @@ import (
 	"fmt"
 	"net"
 
-	"github.com/saeedjhn/go-backend-clean-arch/api/v1/delivery/grpc/userservice"
+	"github.com/saeedjhn/go-backend-clean-arch/api/v1/delivery/grpc/usersvcserver"
+
+	"github.com/saeedjhn/go-backend-clean-arch/configs"
+	"google.golang.org/grpc/reflection"
+
 	"github.com/saeedjhn/go-backend-clean-arch/internal/bootstrap"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 type Server struct {
-	App *bootstrap.Application
+	app *bootstrap.Application
 }
 
 func New(app *bootstrap.Application) *Server {
-	return &Server{App: app}
+	return &Server{app: app}
 }
 
 func (s Server) Run() error {
 
-	addr := fmt.Sprintf(":%s", s.App.Config.GRPCServer.Port)
+	addr := fmt.Sprintf(":%s", s.app.Config.GRPCServer.Port)
 
-	listen, err := net.Listen(s.App.Config.GRPCServer.Network, addr)
+	listen, err := net.Listen(s.app.Config.GRPCServer.Network, addr)
 	if err != nil {
 		return fmt.Errorf(
 			"failed to start listening on %s:%s due to: %w",
-			s.App.Config.GRPCServer.Network,
+			s.app.Config.GRPCServer.Network,
 			addr,
 			err,
 		)
 	}
 
-	s.App.Logger.Set().Named("GRPC.Server").Info("Start.Server", zap.Any("Server.Config", s.App.Config.GRPCServer))
+	s.app.Logger.Set().Named("GRPC.Server").Info("Start.Server", zap.Any("Server.Config", s.app.Config.GRPCServer))
 
 	gs := grpc.NewServer()
 
-	// Register xxxservices
-	userservice.Register(gs)
+	// Register xxxServiceServer
+	usersvcserver.Register(s.app, gs)
 
-	reflection.Register(gs)
+	if s.app.Config.Application.Env != configs.Production {
+		reflection.Register(gs)
+	}
 
 	if err = gs.Serve(listen); err != nil {
 		return fmt.Errorf("gRPC server failed to start serving: %w", err)
