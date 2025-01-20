8950a19cf1192ab32db9a9dae6ac55e2a11a2660
saeedjhn <saeedjahanakbari@gmail.com>
2025-01-20 22:28:14 +0330

    chore: Modified 'response' structure

--- internal/entity/response.go
+++ internal/entity/response.go
@@ -1,41 +1,51 @@
 package entity
 
+import "maps"
+
 type ErrorResponse struct {
-	Success bool          `json:"Success"`
-	Message string        `json:"message"`
-	Errors  []interface{} `json:"errors"`
-	Meta    interface{}   `json:"meta,omitempty"`
+	Success bool                   `json:"Success"`
+	Message string                 `json:"message"`
+	Errors  []interface{}          `json:"errors"`
+	Meta    map[string]interface{} `json:"meta,omitempty"`
 }
 
 func NewErrorResponse(
 	message string,
 	errors interface{},
-	meta ...interface{},
 ) *ErrorResponse {
 	return &ErrorResponse{
 		Success: false,
 		Message: message,
 		Errors:  []interface{}{errors},
-		Meta:    meta,
 	}
 }
 
+func (e *ErrorResponse) WithMeta(meta map[string]interface{}) *ErrorResponse {
+	maps.Copy(e.Meta, meta)
+
+	return e
+}
+
 type SuccessResponse struct {
-	Success bool        `json:"success"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-	Meta    interface{} `json:"meta,omitempty"`
+	Success bool                   `json:"success"`
+	Message string                 `json:"message"`
+	Data    interface{}            `json:"data"`
+	Meta    map[string]interface{} `json:"meta,omitempty"`
 }
 
 func NewSuccessResponse(
 	message string,
 	data interface{},
-	meta ...interface{},
 ) *SuccessResponse {
 	return &SuccessResponse{
 		Success: true,
 		Message: message,
 		Data:    data,
-		Meta:    meta,
 	}
+}
+
+func (e *SuccessResponse) WithMeta(meta map[string]interface{}) *SuccessResponse {
+	maps.Copy(e.Meta, meta)
+
+	return e
 }
