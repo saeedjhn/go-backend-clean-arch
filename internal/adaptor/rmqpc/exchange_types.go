677c1e5414290e3545f24f70252019ac90cda70c
saeedjhn <saeedjahanakbari@gmail.com>
2025-03-24 20:56:30 +0330

    chore: Implement 'rmqpc' package

---
+++ internal/adaptor/rmqpc/exchange_types.go
@@ -0,0 +1,31 @@
+package rmqpc
+
+// ExchangeType defines the type of exchange in RabbitMQ.
+// An exchange determines how messages are routed to queues based on various rules.
+// The type of exchange determines the routing mechanism.
+type ExchangeType string
+
+const (
+	// DirectExchange routes messages to queues based on an exact matching between the message's routing key and the
+	// queue's binding key. This type is used when you need to deliver messages to specific queues based on exact
+	// routing keys.
+	DirectExchange ExchangeType = "direct"
+
+	// FanoutExchange routes messages to all bound queues regardless of the routing key.
+	// This type is typically used for broadcasting messages to all queues.
+	FanoutExchange ExchangeType = "fanout"
+
+	// TopicExchange routes messages to queues based on pattern matching between the routing key and the queue's
+	// binding key. This allows for more complex routing, including wildcards.
+	TopicExchange ExchangeType = "topic"
+
+	// HeadersExchange routes messages based on matching message headers instead of the routing key.
+	// This is useful when routing decisions are based on attributes or metadata in the headers.
+	HeadersExchange ExchangeType = "headers"
+)
+
+// String returns the string representation of the ExchangeType.
+// This is useful for displaying the type as a string or for logging purposes.
+func (e ExchangeType) String() string {
+	return string(e)
+}
---
+++ internal/adaptor/rmqpc/rmqpc_test/rmqpc_test.go
@@ -0,0 +1,106 @@
+package rmqpc_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/saeedjhn/go-backend-clean-arch/internal/contract"
+
+	"github.com/saeedjhn/go-backend-clean-arch/internal/adaptor/rmqpc"
+)
+
+//go:generate go test -v -race -count=1 ./...
+
+func TestConsumeMessage_WhenMessageIsPublished_ShouldReceiveMessagee(t *testing.T) {
+	urTopic := contract.Topic("user.registered")
+
+	cfg := rmqpc.Config{
+		Connection: rmqpc.ConnectionConfig{
+			Username:         "admin",
+			Password:         "password123",
+			Host:             "localhost",
+			Port:             "5672",
+			BaseRetryTimeout: 2 * time.Second,
+			Multiplier:       2.0,
+			MaxDelay:         10 * time.Second,
+			MaxRetry:         5,
+		},
+		MQ: rmqpc.MQConfig{
+			Exchange: rmqpc.ExchangeConfig{
+				Name:       "my-exchange",
+				Kind:       rmqpc.TopicExchange,
+				Durable:    true,
+				AutoDelete: false,
+				Internal:   false,
+				NoWait:     false,
+				Args:       nil,
+			},
+			QueueBind: rmqpc.QueueBindConfig{
+				Queue:            "test-queue",
+				BindingKey:       []contract.Topic{urTopic},
+				Durable:          true,
+				AutoDelete:       false,
+				Exclusive:        false,
+				NoWait:           false,
+				ArgsQueueDeclare: nil,
+				ArgsQueueBind:    nil,
+				PrefetchCount:    1,
+				PrefetchSize:     0,
+				PrefetchGlobal:   false,
+			},
+			Publish: rmqpc.PublishConfig{
+				Mandatory: false,
+				Immediate: false,
+			},
+			Consume: rmqpc.ConsumeConfig{
+				AutoAck:   false,
+				Exclusive: false,
+				NoLocal:   false,
+				NoWait:    false,
+				Args:      nil,
+			},
+		},
+	}
+
+	rMQ := rmqpc.New("my-connection", cfg)
+
+	if err := rMQ.Connect(); err != nil {
+		t.Fatalf("Failed to connect: %v", err)
+	}
+
+	if err := rMQ.SetupExchange(); err != nil {
+		t.Fatalf("Failed to setup exchange: %v", err)
+	}
+
+	if err := rMQ.SetupBindQueue(); err != nil {
+		t.Fatalf("Failed to setup queue binding: %v", err)
+	}
+
+	eventStream := make(chan contract.Event)
+
+	go func() {
+		err := rMQ.Consume(eventStream)
+		if err != nil {
+			t.Errorf("Error consuming messages: %v", err)
+			return
+		}
+	}()
+
+	time.Sleep(2 * time.Second)
+	_ = rMQ.Publish(contract.Event{Topic: "user.registered", Payload: []byte("New User Registered")})
+
+	// 	for evt := range eventStream {
+	// 		t.Logf("Received evt: Topic=%s, Payload=%s", evt.Topic, string(evt.Payload))
+	// 	}
+
+	for {
+		select {
+		case evt := <-eventStream:
+			t.Logf("Received evt: Topic=%s, Payload=%s", evt.Topic, string(evt.Payload))
+			return
+		case <-time.After(5 * time.Second):
+			t.Fatalf("Test timed out waiting for message")
+			return
+		}
+	}
+}
